f6fc1dd705db89b47cc9df5762bcb81d49258fb5
raphael.champeaud <raphael.champeaud@gmail.com>
2018-12-04 19:29:08 +0100

    add spam + test config
    # Conflicts:
    #	config.json

---
+++ commands/spam.js
@@ -0,0 +1,11 @@
+exports.run = (client, message) => {
+  if (message.mentions.members.size === 0) {
+    return message.channel.send('Please mention a user');
+  }
+  for( let i =0; i< 20; i++){
+    message.channel.send(`${message.mentions.members.first()}`)
+      .then(message => {
+        message.react('❤');
+      }).catch(() => {});
+  }
+};
